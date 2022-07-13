727002f57fb3f056b99db673a49990290d1d8299
Richard Chang <rchang24@seas.upenn.edu>
2022-07-13 16:32:14 -0400

    added multigoal functionality

--- envs/sibrivalry/ant_maze/__init__.py
+++ envs/sibrivalry/ant_maze/__init__.py
@@ -51,7 +51,6 @@ class AntMazeEnv(gym.GoalEnv):
       else:
         raise ValueError('Bad maze name!')
 
-
     self.maze = create_maze_env(mazename) # this returns a gym environment
     self.seed()
     self.max_steps = 500
@@ -254,15 +253,15 @@ class AntMazeEnvFull(gym.GoalEnv):
 class AntMazeEnvFullDownscale(gym.GoalEnv):
   """Wraps the HIRO/SR Ant Environments in a gym goal env."""
   def __init__(self, variant='AntMazeFullDownscale-SR', eval=False):
-
     self.done_env = False
     if eval:
       self.dist_threshold = 5.0
     else:
       self.dist_threshold = np.sqrt(2)
     state_dims = 29
-    
-    
+    self.goal_list = []
+    self.goal_list = [[0.00, 4.20], [4.20, 4.20], [2.25, 4.20], [4.20, 2.25], [4.20, 0.00]]
+    self.goal_idx = 0
     mazename = variant.split('-')
     if len(mazename) == 2:
       mazename, test_goals = mazename
@@ -346,16 +345,28 @@ class AntMazeEnvFullDownscale(gym.GoalEnv):
     _ = self.maze.wrapped_env.seed(self.np_random.randint(np.iinfo(np.int32).max))
     s = self.maze.reset().astype(np.float32)
     _ = self.maze.wrapped_env.seed(self.np_random.randint(np.iinfo(np.int32).max))
-    other_dims = np.array([8.19890515e-01,  9.95145602e-01,
-        3.48547286e-02, -6.19350100e-02,  6.80766655e-02, -4.42372144e-02,
-       -4.81461428e-02,  1.45511675e-02, -7.75746132e-02,  5.00618279e-02,
-        3.65949561e-02,  4.99939194e-02,  1.02664477e-02, -2.27597298e-01,
-        8.01758031e-02, -8.81610163e-02,  7.77121806e-02,  3.36722131e-02,
-        2.27648027e-02, -2.24103019e-02, -3.77942221e-02, -6.56355237e-02,
-        1.35722257e-01,  6.93039877e-02, -1.71162114e-01, -1.12083335e-01,
-        1.76819156e-02])
-    self.g_xy = np.concatenate((self.sample_goal(), other_dims))
-
+    # other_dims = np.array([8.19890515e-01,  9.95145602e-01,
+    #     3.48547286e-02, -6.19350100e-02,  6.80766655e-02, -4.42372144e-02,
+    #    -4.81461428e-02,  1.45511675e-02, -7.75746132e-02,  5.00618279e-02,
+    #     3.65949561e-02,  4.99939194e-02,  1.02664477e-02, -2.27597298e-01,
+    #     8.01758031e-02, -8.81610163e-02,  7.77121806e-02,  3.36722131e-02,
+    #     2.27648027e-02, -2.24103019e-02, -3.77942221e-02, -6.56355237e-02,
+    #     1.35722257e-01,  6.93039877e-02, -1.71162114e-01, -1.12083335e-01,
+    #     1.76819156e-02])
+    other_dims = np.concatenate([[6.08193526e-01,  9.87496030e-01,
+  1.82685311e-03, -6.82827458e-03,  1.57485326e-01,  5.14617396e-02,
+  1.22386603e+00, -6.58701813e-02, -1.06980319e+00,  5.09069276e-01,
+ -1.15506861e+00,  5.25953435e-01,  7.11716520e-01], np.zeros(14)])
+    if len(self.goal_list) > 0:
+      print(self.maze.wrapped_env.sim.data.qpos[:2])
+      self.g_xy = np.concatenate((self.goal_list[self.goal_idx], other_dims))
+      # self.maze.wrapped_env.set_state(self.g_xy[:15], self.g_xy[15:])
+      # self.maze.wrapped_env.sim.forward()
+      # img = self.maze.render("rgb_array")
+      # import imageio
+      # imageio.imwrite('goal_view.png', img)
+    else:
+      self.g_xy = np.concatenate((self.sample_goal(), other_dims))
     return {
       'observation': s,
       'achieved_goal': s[self.goal_dims],
@@ -374,3 +385,11 @@ class AntMazeEnvFullDownscale(gym.GoalEnv):
       dg = desired_goal[self.eval_dims]
     d = np.linalg.norm(ag - dg, axis=-1)
     return -(d >= self.dist_threshold).astype(np.float32)
+
+  def set_goal_idx(self, idx):
+    assert len(self.goal_list) > 0
+    self.goal_idx = idx
+  
+  def get_goals(self):
+    return self.goal_list
+    
